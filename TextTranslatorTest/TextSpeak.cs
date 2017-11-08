8d945b910938fb188bfec2f047a0ac41bc745706
Tomokazu Kizawa <tomokazu.kizawa@outlook.com>
2017-11-08 20:23:28 +0900

    テキストとボタンを追加 TextSpeakクラスを追加

---
+++ TextTranslatorTest/TextSpeak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTranslatorTest
+{
+    class TextSpeak
+    {
+        private const string Key1 = "6f6a4a6d7ba644f493f71272ea6a30b6";
+    }
+}
